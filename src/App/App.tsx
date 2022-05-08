2846a255b8a8b4e18fcc1727ea27706f24b08feb
Egor Shevchuk <shevchukea@msk.bcs.ru>
2022-05-08 20:55:15 +0300

    init

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import style from "./App.module.sass";
+
+function App() {
+  return (
+    <div className={style.App}>
+      <header className={style.appHeader}>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
